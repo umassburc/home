2648f1ddf789e9c98184c95dad131b86dfee29e1
umassburc <155315227+umassburc@users.noreply.github.com>
2023-12-31 15:57:25 -0500

    Add files via upload

---
+++ back-to-top.js
@@ -0,0 +1,21 @@
+
+var mybutton = document.getElementById("back-to-top-btn");
+
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
